1bfe140b3c38ff57de4ce1b760ad919e4b5cac78
112gaza <alangilford@icloud.com>
2023-12-15 12:42:04 +0100

    deploy

---
+++ _next/static/chunks/app/(plattform)/layout-49a9cd6c5b6571f9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[146],{4727:function(e,n,t){Promise.resolve().then(t.t.bind(t,8326,23)),Promise.resolve().then(t.t.bind(t,3994,23)),Promise.resolve().then(t.t.bind(t,5093,23)),Promise.resolve().then(t.t.bind(t,8117,23))},5093:function(){}},function(e){e.O(0,[428,454,971,472,744],function(){return e(e.s=4727)}),_N_E=e.O()}]);
